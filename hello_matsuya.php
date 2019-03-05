c4c87638e8d35536fec4ad5e18a62600e6c07b9f
TinyKitten <catwalk8205@gmail.com>
2019-03-05 23:33:03 +0900

    first commit

---
+++ hello_matsuya.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Hello_matsuya
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     hello_matsuya
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hello_matsuya
+ */
+
+// Your code starts here.
