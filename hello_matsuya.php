e2c99949e2d5ff84513e0ea4590c651922fdd377
TinyKitten <catwalk8205@gmail.com>
2019-03-05 23:41:23 +0900

    plugin情報更新

--- hello_matsuya.php
+++ hello_matsuya.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name:     Hello_matsuya
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin Name:     Hello Matsuya
+ * Plugin URI:      https://github.com/TinyKitten/WPHelloMatsuya
+ * Description:     これはただのプラグインではありません。まこてぃあ(@hs6a) によって作られた最も有名なAPI、Matsuya-Web-API に要約された同一世代のすべての人々の希望と情熱を象徴するものです。このプラグインを有効にすると、すべての管理画面の右上に 松屋のメニューがランダムに表示されます。
+ * Author:          TinyKitten
+ * Author URI:      https://tinykitten.me
  * Text Domain:     hello_matsuya
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,3 @@
  * @package         Hello_matsuya
  */
 
-// Your code starts here.
